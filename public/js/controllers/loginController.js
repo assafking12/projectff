9fb55b093e42eb21d31e1f7c47b318de3fee5de1
Assaf Zvigoren <assafking12@gmail.com>
2016-10-14 14:19:04 +0300

    dealing with facebook async loading

--- public/js/controllers/loginController.js
+++ public/js/controllers/loginController.js
@@ -16,21 +16,26 @@ myapp.controller('loginCtrl', function ($scope, $http) {
         }
     }
 
-    // window.fbAsyncInit = function () {
-    //
-    //
-    //
-    // };
+    window.fbAsyncInit = function () {
+        FB.init({
+            appId: '292046017808004',    // Prod
+            // appId: '236265666776812',       //Test
+            status: true,
+            cookie: true,
+            xfbml: true,
+            version: 'v2.4'
+        });
 
-    FB.getLoginStatus(function (response) {
-        if (response.status === 'connected') {
-            var date = new Date();
-            var millis = date.getTime() + response.authResponse.expiresIn * 1000;
-            date = new Date(millis - date.getTimezoneOffset() * 60000);
-            var expirationDate = date.toISOString();
-            saveUser(expirationDate, response);
-        }
-    });
+        FB.getLoginStatus(function (response) {
+            if (response.status === 'connected') {
+                var date = new Date();
+                var millis = date.getTime() + response.authResponse.expiresIn * 1000;
+                date = new Date(millis - date.getTimezoneOffset() * 60000);
+                var expirationDate = date.toISOString();
+                saveUser(expirationDate, response);
+            }
+        });
+    };
 
     function saveUser(expirationDate, response) {        
         $scope.user.id = response.authResponse.userID;
