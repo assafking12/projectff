4d00f1cfd41e0d5d447791b3837c2f2fd6f20f20
Assaf Zvigoren <assafking12@gmail.com>
2016-10-14 13:49:14 +0300

    dealing with facebook async loading

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
-// var url = "mongodb://AssafZvigi:Aa123456@ds011462.mlab.com:11462/projectff"; // Prod
-var url = "mongodb://localhost:27017/projectff"; // Test
+var url = "mongodb://AssafZvigi:Aa123456@ds011462.mlab.com:11462/projectff"; // Prod
+// var url = "mongodb://localhost:27017/projectff"; // Test
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
