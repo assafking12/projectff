445e70cd1d871ad140993a743ec7eb843478e403
assafking12 <assafking12@gmail.com>
2016-10-19 01:18:25 +0300

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-//app.use(bodyParser({limit: '50mb'}));
+app.use(bodyParser({limit: '50mb'}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -78,5 +78,5 @@ app.use(function(err, req, res, next) {
 
 module.exports = app;
 
-app.listen(3000);
+//app.listen(3000);
 // console.log('Listening on port 3000');
